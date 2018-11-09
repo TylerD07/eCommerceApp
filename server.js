29b3bd8ea83fd03002686f0c7ec91f864f1e6c78
TylerD07 <tyler.daughtry@aol.com>
2018-11-09 14:14:03 -0500

    configured heroku and JAWSBD

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const path = require('path');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const db = require("./models");
 const app = express();
 
